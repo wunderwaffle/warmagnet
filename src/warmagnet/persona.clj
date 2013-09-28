416f4cb3937598b790de7c7b0ad03548551ccf30
Roman Semenenko <volh@ginnungagap.in.ua>
2013-09-28 17:17:22 +0300

    resolve promise

--- src/warmagnet/persona.clj
+++ src/warmagnet/persona.clj
@@ -9,8 +9,8 @@
 	(hk/post url {:form-params params} #(go (>! result %))))
 
 (defn login [token]
-	(let [response (hk/post "https://verifier.login.persona.org/verify"
-                            {:assertion token :audience "http://localhost:3000"})]
+	(let [response @(hk/post "https://verifier.login.persona.org/verify"
+                            {:form-params {:assertion token :audience "http://localhost:3000"}})]
 		(json/decode (:body response) true)))
 
 ;(defn login [token] {:status "okay" :email "alexander@solovyov.net"})
