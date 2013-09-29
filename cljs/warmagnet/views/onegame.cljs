06d4c0af5b7896a4a60f460d7e5f9aec4d8295d3
Roman Semenenko <volh@ginnungagap.in.ua>
2013-09-29 19:57:03 +0300

    log->text placeholder

--- cljs/warmagnet/views/onegame.cljs
+++ cljs/warmagnet/views/onegame.cljs
@@ -2,6 +2,11 @@
   (:require-macros [pump.def-macros :refer [defr]])
   (:require [warmagnet.views.gamemap :refer [GameMap]]))
 
+(defn log->text [log]
+  (case (:type log)
+    "join" "User " (:user-name log) " joined the game"
+    "Event: " (:type log) " by " (:user-name log)))
+
 (defr Game
   [C {:keys [map game]} S]
   [:div
