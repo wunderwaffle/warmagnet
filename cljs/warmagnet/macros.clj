ba8e1c7b70d12c068877b43f1f82641487d6326a
Vsevolod Solovyov <vsevolod.solovyov@gmail.com>
2013-09-29 13:22:07 +0300

    Better cx macro

--- cljs/warmagnet/macros.clj
+++ cljs/warmagnet/macros.clj
@@ -2,5 +2,5 @@
 
 (defmacro cx [& classes]
   (let [class-map# (partition 2 classes)]
-    `(str ~@(for [[class# test#] class-map#]
-              `(if ~test# (str (name ~class#) " ") "")))))
+    `(str ~@(apply concat (for [[class# test#] class-map#]
+                            [`(if ~test# (name ~class#)) " "])))))
