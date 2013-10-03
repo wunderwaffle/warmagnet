157424e5aebe5ffb053d02381e55f3bb2dd2ae51
Vsevolod Solovyov <vsevolod.solovyov@gmail.com>
2013-10-03 15:40:41 +0300

    Rename state changing functions to use !, take few functions to outer scope.

--- cljs/warmagnet/views/gamemap.cljs
+++ cljs/warmagnet/views/gamemap.cljs
@@ -43,8 +43,26 @@
   (COLORS
    (player-index game user-id)))
 
+(defn deploy!
+  [game-id district amount]
+  (send-log game-id {:type :deploy :district district :amount amount}))
+
+(defn attack!
+  [game-id amount from to]
+  (send-log game-id {:type :attack
+                     :amount amount
+                     :attack-from from
+                     :attack-to to}))
+
+(defn reinforce!
+  [game-id amount from to]
+  (send-log game-id {:type :reinforce
+                     :amount amount
+                     :reinforce-from from
+                     :reinforce-to to}))
+
 (defr MapDistrict
-  [C {:keys [district dname amount click selected assoc-hovered color]} S]
+  [C {:keys [district dname amount click selected hovered! color]} S]
   
   (let [{:keys [borders coordinates]} (second district)
         [x y] coordinates]
@@ -58,8 +76,8 @@
                  :label-danger (= selected :target))
       :style (clj->js {:left x :top y :color color})
       :on-click (if click (fn [C] (click district)) #())
-      :on-mouse-enter #(assoc-hovered district)
-      :on-mouse-leave #(assoc-hovered nil)}
+      :on-mouse-enter #(hovered! district)
+      :on-mouse-leave #(hovered! nil)}
      amount]))
 
 (defr Deploy
@@ -67,14 +85,14 @@
   (fn [C {:keys [available-troops]} S]
     (assoc-in-state C :to-deploy available-troops))
   
-  [C {:keys [available-troops district deploy!]} {:keys [to-deploy]}]
+  [C {:keys [available-troops district game-id]} {:keys [to-deploy]}]
   (let [[dname {:keys [coordinates]}] district
         [x y] (xy-for-popover coordinates)]
     [:div.popover {:style (clj->js {:display "block" :left x :top y})}
      [:div.popover-content
       [:div.input-group
        [:span.input-group-btn
-        [:button.btn.btn-success {:on-click #(deploy! district to-deploy)} "Deploy"]]
+        [:button.btn.btn-success {:on-click #(deploy! game-id dname to-deploy)} "Deploy"]]
        [:input.form-control
         {:on-change #(if-let [v (js/parseInt (e-value %))]
                        (if (<= 1 v available-troops)
@@ -88,7 +106,7 @@
        [:span.input-group-addon available-troops]]]]))
 
 (defr Attack
-  [C {:keys [attacker attacking defender defending attack!]} S]
+  [C {:keys [attacker attacking defender defending game-id]} S]
   (let [[aname {:keys [coordinates]}] attacker
         [dname dmap] defender
         [x y] (xy-for-popover coordinates)]
@@ -98,14 +116,14 @@
       [:thead [:tr [:th (name aname)] [:th (name dname)]]]
       [:tbody [:tr [:td attacking] [:td defending]]]]
      [:div.btn-group
-      [:button.btn.btn-warning {:on-click #(attack! 1 aname dname)} "Attack"]
-      [:button.btn.btn-danger  {:on-click #(attack! (dec attacking) aname dname)} "Blitz"]]]]))
+      [:button.btn.btn-warning {:on-click #(attack! game-id 1 aname dname)} "Attack"]
+      [:button.btn.btn-danger  {:on-click #(attack! game-id (dec attacking) aname dname)} "Blitz"]]]]))
 
 (defr Reinforce
   :component-will-mount (fn [C P S] (assoc-in-state C :transfer 0))
 
   [C
-   {:keys [dst-from troops-from dst-to troops-to reinforce!]}
+   {:keys [dst-from troops-from dst-to troops-to game-id]}
    {:keys [transfer]}]
   (let [[fname {:keys [coordinates]}] dst-from
         [tname tmap] dst-to
@@ -137,12 +155,13 @@
   ((game-districts (name dname)) :amount))
 
 (defr GameMap
-  :component-will-mount (fn [C P] (if-not (:name P) (xhr-load-map)))
+  :component-will-mount (fn [C P] (if-not (:map-src P) (xhr-load-map)))
   :get-initial-state (fn [] {})
 
   [C
    {:keys [game-map game user container-width]}
-   {:keys [hovered deploying attacker defender]}]
+   {:keys [hovered deploying attacker defender]}
+   ]
   (let [{:keys [map-src regions districts dimensions]} game-map
         {:keys [player-state players turn-by]} game
         game-districts (:districts game)
@@ -150,30 +169,26 @@
         user-id (:id user)
         is-my-turn (= turn-by user-id)
         phase (keyword (:phase (user-state game user)))
-   
-        clear-popovers #(assoc-state C {:attacker nil :deploying nil :defender nil})
-        assoc-hovered #(assoc-in-state C :hovered %)
+
+        clear-popovers! #(assoc-state C {:attacker nil :deploying nil :defender nil})
+        hovered! #(assoc-in-state C :hovered %)
 
         dst-deploy #(assoc-in-state C :deploying (if (not= deploying %) % nil))
         dst-attacker #(if (= % attacker)
-                        (clear-popovers)
+                        (clear-popovers!)
                         (assoc-in-state C :attacker %))
         dst-defender #(if (attack? attacker %)
                         (assoc-in-state C :defender %))
         dst-reinforce #(cond
-                        (= % attacker) (clear-popovers)
+                        (= % attacker) (clear-popovers!)
                         (nil? attacker) (assoc-in-state C :attacker %)
                         (reinforce? attacker %) (assoc-in-state C :defender %))
 
-        deploy! #(send-log game-id {:type :deploy :district (first %1) :amount %2})
-        attack! #(send-log game-id {:type :attack :amount %1 :attack-from %2 :attack-to %3})
-        reinforce! #(assoc-state C {:attacker nil :defender nil :state :deploy})
-
         district-action #(if is-my-turn 
-                          (case phase
-                            :deploy (if (= % user-id) dst-deploy)
-                            :attack (if (= % user-id) dst-attacker dst-defender)
-                            :reinforce (if (= % user-id) dst-reinforce)))
+                           (case phase
+                             :deploy (if (= % user-id) dst-deploy)
+                             :attack (if (= % user-id) dst-attacker dst-defender)
+                             :reinforce (if (= % user-id) dst-reinforce)))
 
         selection-for #(condp = %1
                          hovered :hover
@@ -183,34 +198,35 @@
                          (cond
                           (and (= phase :reinforce) (= %2 user-id)
                                attacker (reinforce? attacker %1)) :neighbor
-                          (and (= phase :attack) (not= %2 user-id)
-                               attacker (attack? attacker %1)) :target))]
+                               (and (= phase :attack) (not= %2 user-id)
+                                    attacker (attack? attacker %1)) :target))]
 
     (if-not map-src
       [:div "No map"]
       [:div.game-map
        {:style (get-map-style dimensions container-width)}
        [:img {:src (str "/static/" map-src)
-              :on-click clear-popovers}]
+              :on-click clear-popovers!}]
        (map (fn [district]
               (let [[dname map-district] district
                     gd (game-districts (name dname))]
                 [MapDistrict {:district district
                               :dname dname
                               :selected (selection-for district (gd :user-id))
-                              :assoc-hovered assoc-hovered
+                              :hovered! hovered!
                               :amount (gd :amount)
                               :color (player-color game (gd :user-id))
                               :click (district-action (gd :user-id))}]))
             districts)
        (if (and (= phase :deploy) deploying)
          [Deploy {:available-troops (:supply (user-state game user))
-                  :district deploying :deploy! deploy!}])
+                  :district deploying :game-id game-id}])
        (if (and (= phase :attack) attacker defender)
          [Attack {:attacker attacker :attacking (troops-on attacker game-districts)
                   :defender defender :defending (troops-on defender game-districts)
-                  :attack! attack!}])
+                  :game-id game-id}])
        (if (and (= phase :reinforce) attacker defender)
          [Reinforce {:dst-from attacker :troops-from (troops-on attacker game-districts)
                      :dst-to defender :troops-to (troops-on defender game-districts)
-                     :reinforce! reinforce!}])])))
+                     :game-id game-id}])])))
+
